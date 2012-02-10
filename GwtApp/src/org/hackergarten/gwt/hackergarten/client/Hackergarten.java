5bd014820f871021729bf9dc39dac5883ce52321
Hamlet D'Arcy <hamletdrc@gmail.com>
2012-02-10 21:27:49 +0100

    hackergarten gwt app - base application template

--- GwtApp/src/org/hackergarten/gwt/hackergarten/client/Hackergarten.java
+++ GwtApp/src/org/hackergarten/gwt/hackergarten/client/Hackergarten.java
@@ -2,7 +2,6 @@ package org.hackergarten.gwt.hackergarten.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.*;
-import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -12,25 +11,51 @@ import com.google.gwt.event.dom.client.ClickEvent;
  */
 public class Hackergarten implements EntryPoint {
 
-	/**
-	 * This is the entry point method.
-	 */
-	public void onModuleLoad() {
-		final Button button = new Button("Click me");
-		final Label label = new Label();
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+        final Button buttonHome = new Button("Home");
+        final Button buttonCheckin = new Button("Checkin");
+        final Button buttonRegister = new Button("Register");
+        RootPanel buttonHolder = RootPanel.get("buttonholder");
+        final RootPanel panel1 = RootPanel.get("slot1");
+        final RootPanel panel2 = RootPanel.get("slot2");
+        final RootPanel panel3 = RootPanel.get("slot3");
 
-		button.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
+//        hidePanels();
+//        panel1.setVisible(true);
 
-			}
-		});
+        buttonHolder.add(buttonHome);
+        buttonHolder.add(buttonCheckin);
+        buttonHolder.add(buttonRegister);
 
-		// Assume that the host HTML has elements defined whose
-		// IDs are "slot1", "slot2".  In a real app, you probably would not want
-		// to hard-code IDs.  Instead, you could, for example, search for all
-		// elements with a particular CSS class and replace them with widgets.
-		//
-		RootPanel.get("slot1").add(button);
-		RootPanel.get("slot2").add(label);
-	}
+        buttonHome.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                hidePanels();
+                panel1.setVisible(true);
+            }
+        });
+
+        buttonCheckin.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                hidePanels();
+                panel2.setVisible(true);
+            }
+        });
+
+        buttonRegister.addClickHandler(new ClickHandler() {
+            public void onClick(ClickEvent event) {
+                hidePanels();
+                panel3.setVisible(true);
+            }
+        });
+
+    }
+    public void hidePanels() {
+        RootPanel.get("slot1").setVisible(false);
+        RootPanel.get("slot2").setVisible(false);
+        RootPanel.get("slot3").setVisible(false);
+    }
+    
 }
